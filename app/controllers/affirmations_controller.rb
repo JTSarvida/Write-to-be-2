a11e1613914d4fab943b9f5049bc900a91bb2734
Joshua Tan <jtsarvida@gmail.com>
2019-02-08 04:50:49 -0500

    Added get '/affirmations' and get '/affirmations/new' in the AffirmationsController. No session logic yet.

--- app/controllers/affirmations_controller.rb
+++ app/controllers/affirmations_controller.rb
@@ -1,3 +1,12 @@
 class AffirmationsController < ApplicationController
 
+  get '/affirmations' do
+    erb :'/affirmations'
+  end
+
+  get '/affirmations/new' do
+    erb :'/affirmations/new'
+  end
+
+
 end

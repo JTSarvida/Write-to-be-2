496c8760aa424fd540dfe9712a9def8c290ce816
Joshua Tan <jtsarvida@gmail.com>
2019-02-11 05:53:05 -0500

    Removed "match-id" and "likes" from the two migrations, will add possibly later.

--- db/schema.rb
+++ db/schema.rb
@@ -20,10 +20,9 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "email"
-    t.string  "password_digest"
-    t.integer "match_id"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
